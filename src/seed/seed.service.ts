7b0c21426e6bedf51fe32656f04a526006d48a56
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2024-11-27 15:31:00 -0500

    clase 115: paso 4 - crear items

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -7,6 +7,7 @@ import { User } from './../users/entities/user.entity';
 import { Item } from './../items/entities/item.entity';
 
 import { UsersService } from './../users/users.service';
+import { ItemsService } from './../items/items.service';
 
 import { SEED_ITEMS, SEED_USERS } from './data/seed-data';
 
@@ -16,11 +17,15 @@ export class SeedService {
 
     constructor(
         private readonly configService: ConfigService,
+
         @InjectRepository(Item)
         private readonly itemsRepository: Repository<Item>,
+
         @InjectRepository(User)
         private readonly userRepository: Repository<User>,
+
         private readonly usersService: UsersService,
+        private readonly itemsService: ItemsService,
     ) {
         this.isProd = this.configService.get('STATE') === 'prod';
     }
@@ -33,11 +38,9 @@ export class SeedService {
 
             await this.deleteDatabase();
 
-            //Crear usuario
+            const user = await this.loadUsers();
 
-            await this.loadUsers();
-
-            //Crear items
+            await this.loadItems(user);
 
             return true;
         } catch (error) {
@@ -59,23 +62,27 @@ export class SeedService {
             .execute();
     }
 
-    async loadUsers(): Promise<User[]> {
+    async loadUsers(): Promise<User> {
         const users = [];
 
         for (const user of Object.values(SEED_USERS)) {
             users.push(await this.usersService.createUser(user));
         }
 
-        return await this.userRepository.save(users);
+        await this.userRepository.save(users);
+        return users[0];
     }
 
-    async loadItems(): Promise<Item[]> {
-        const items = [];
+    async loadItems(user: User): Promise<void> {
+        const itemsPromises = [];
 
         for (const item of Object.values(SEED_ITEMS)) {
-            items.push(this.itemsRepository.create(item));
+            itemsPromises.push(this.itemsService.create(item, user));
         }
 
-        return await this.itemsRepository.save(items);
+        await Promise.all(itemsPromises);
+
+        //await this.itemsRepository.save(items);
+        //return await this.itemsRepository.save(items);
     }
 }
